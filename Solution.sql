6e0165f258a64137cbaf858fa5d4f0618e9d3e98
Nitin <nitinmagdum111@gmail.com>
2024-09-20 00:19:30 +0530

    Added 2nd solution

--- Solution.sql
+++ Solution.sql
@@ -4,9 +4,12 @@ SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
 ### 1.  Fetch all the paintings which are not displayed on any museums?
 */
 
-select * from public.work where museum_id is NULL
+select * from work where museum_id is NULL
 
 /*markdown
 ### 2.  Are there museums without any paintings?
 */
 
+select * from museum m
+	where not exists (select 1 from work w
+					 where w.museum_id=m.museum_id)
