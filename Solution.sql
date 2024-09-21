4cd8381712c2fd9a5eba201a8c677b7604be0b43
Nitin <nitinmagdum111@gmail.com>
2024-09-21 16:45:05 +0530

    new branch

--- Solution.sql
+++ Solution.sql
@@ -2,12 +2,6 @@
 ### 1.  Fetch all the paintings which are not displayed on any museums?
 */
 
-select * from work where museum_id is NULL
-
 /*markdown
 ### 2.  Are there museums without any paintings?
 */
-
-select * from museum m
-	where not exists (select 1 from work w
-					 where w.museum_id=m.museum_id)
