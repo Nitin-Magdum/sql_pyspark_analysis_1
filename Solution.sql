94aab992a2d18509b0649759367d1a84ef48efe1
Nitin <nitinmagdum111@gmail.com>
2024-09-30 20:10:25 +0530

    updated

--- Solution.sql
+++ Solution.sql
@@ -1,7 +1,8 @@
-/*markdown
-### 1.  Fetch all the paintings which are not displayed on any museums?
-*/
 
-/*markdown
-### 2.  Are there museums without any paintings?
-*/
+select * from work where museum_id is NULL
+
+select * from museum m where not exists (select 1 from work w where w.museum_id=m.museum_id)
+
+select count(*) from product_size where sale_price > regular_price
+
+select * from product_size where sale_price < (regular_price * 0.5)
